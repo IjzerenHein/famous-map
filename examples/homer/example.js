546789d4e2796c8abd26fd99b9041d907907afb3
IjzerenHein <hrutjes@gmail.com>
2014-06-07 19:47:04 +0200

    Example: Updated homer example to use the new map-modifiers.

--- examples/homer/example.js
+++ examples/homer/example.js
@@ -35,10 +35,13 @@ define(function (require) {
     var Modifier = require('famous/core/Modifier');
     var Surface = require('famous/core/Surface');
     var ImageSurface = require('famous/surfaces/ImageSurface');
+    var Transform = require('famous/core/Transform');
     var Easing = require('famous/transitions/Easing');
     var Timer = require('famous/utilities/Timer');
     var MapView = require('famous-map/MapView');
-    var MapItem = require('famous-map/MapItem');
+    var Map = require('famous-map/MapItem');
+    var MapItemModifier = require('famous-map/MapItemModifier');
+    var MapItemStateModifier = require('famous-map/MapItemStateModifier');
 
     // create the main context
     var mainContext = Engine.createContext();
@@ -48,13 +51,15 @@ define(function (require) {
     
     // Create map-view
     var mapView = _createMapView(mainContext);
+    
 
     // Wait for the map to load and initialize
     mapView.on('load', function () {
 
         // Create map items
         var homer = _createHomer(mainContext, mapView);
-        _createMarge(mainContext, mapView);
+        _createArrow(mainContext, mapView, homer);
+        _createMarge(mainContext, mapView, homer);
         _createPins(mainContext, mapView);
         _createSaucer(mainContext, mapView);
 
@@ -65,35 +70,50 @@ define(function (require) {
         mapView.setZoom(14);
         
         // Homer is taking a road-trip
-        homer.rotateTowards(new google.maps.LatLng(51.8721795, 5.7101037), { duration: 500, curve: Easing.outQuad });
-        homer.setPosition(new google.maps.LatLng(51.8721795, 5.7101037), { duration: 2000, curve: Easing.outQuad });
-        homer.rotateTowards(new google.maps.LatLng(51.4400867, 5.4782571), { duration: 500, curve: Easing.outQuad });
-        homer.setPosition(new google.maps.LatLng(51.4400867, 5.4782571), { duration: 4000, curve: Easing.outQuad }, function () {
-            
-            // Drive circles around town until homer gets tired of it
-            var i, j, roundabout = [
-                new google.maps.LatLng(51.4347897, 5.452068),
-                new google.maps.LatLng(51.4470413, 5.4474332),
-                new google.maps.LatLng(51.4524705, 5.4941894),
-                new google.maps.LatLng(51.4383345, 5.5051329),
-                new google.maps.LatLng(51.4284487, 5.5016138),
-                new google.maps.LatLng(51.4237288, 5.4911202),
-                new google.maps.LatLng(51.4250333, 5.474351),
-                new google.maps.LatLng(51.4286323, 5.4603713),
-                new google.maps.LatLng(51.4315555, 5.4541915)
-            ];
-            for (i = 0; i < 10; i++) {
-                for (j = 0; j < roundabout.length; j++) {
-                    homer.rotateTowards(roundabout[j], { duration: 100, curve: Easing.outQuad });
-                    homer.setPosition(roundabout[j], { duration: 2000 });
+        var i, j, trip = [
+            new google.maps.LatLng(51.8721795, 5.7101037),
+            new google.maps.LatLng(51.4400867, 5.4782571)
+        ];
+        homer.rotateTowards(trip[0], { duration: 200 });
+        for (j = 0; j < trip.length; j++) {
+            homer.setPosition(trip[j], { duration: 4000, curve: Easing.outQuad }, function (j) {
+                if ((j + 1) < trip.length) {
+                    homer.rotateTowards(trip[j + 1], { duration: 200 });
                 }
+            }.bind(this, j));
+        }
+        
+        // Drive circles around town until homer gets tired of it
+        var roundabout = [
+            new google.maps.LatLng(51.4347897, 5.452068),
+            new google.maps.LatLng(51.4470413, 5.4474332),
+            new google.maps.LatLng(51.4520125, 5.4643767),
+            new google.maps.LatLng(51.4524705, 5.4941894),
+            new google.maps.LatLng(51.4383345, 5.5051329),
+            new google.maps.LatLng(51.4284487, 5.5016138),
+            new google.maps.LatLng(51.4237288, 5.4911202),
+            new google.maps.LatLng(51.4250333, 5.474351),
+            new google.maps.LatLng(51.4286323, 5.4603713),
+            new google.maps.LatLng(51.4315555, 5.4541915)
+        ];
+        homer.rotateTowards(roundabout[0]);
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < roundabout.length; j++) {
+                //homer.rotateTowards(roundabout[j], { duration: 100, curve: Easing.outQuad });
+                homer.setPosition(roundabout[j], { duration: 2000 }, function (j) {
+                    if ((j + 1) < roundabout.length) {
+                        homer.rotateTowards(roundabout[j + 1], { duration: 200 });
+                    } else {
+                        homer.rotateTowards(roundabout[0], { duration: 200 });
+                    }
+                }.bind(this, j));
             }
-                        
-            // Show info-box with a slight delay
-            Timer.setTimeout(function () {
-                _createInfoBox(mainContext, mapView);
-            }, 4000);
-        });
+        }
+
+        // Show info-box with a slight delay
+        Timer.setTimeout(function () {
+            _createInfoBox(mainContext, mapView);
+        }, 8000);
     });
     
     /**
@@ -143,6 +163,35 @@ define(function (require) {
     }
     
     /**
+     * @method _createArrow
+     * @private
+     */
+    function _createArrow(context, mapView, homer) {
+        
+        // Create the homer-mobile which travels across the map
+        var surface = new ImageSurface({
+            size: [true, true],
+            classes: ['arrow'],
+            content: '../content/images/arrow.png'
+        });
+        var center = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5]
+        });
+        var rotation = new Modifier({
+            transform: Transform.rotateZ(Math.PI / 2)
+        });
+        var modifier = new MapItemModifier({
+            mapView: mapView,
+            position: new google.maps.LatLng(51.4367399, 5.4812397),
+            rotateTowards: homer,
+            zoomBase: 14
+        });
+        context.add(modifier).add(center).add(rotation).add(surface);
+        return modifier;
+    }
+    
+    /**
      * @method _createHomer
      * @private
      */
@@ -150,35 +199,43 @@ define(function (require) {
         
         // Create the homer-mobile which travels across the map
         var surface = new ImageSurface({
-            size: [50, true],
+            size: [80, true],
             classes: ['car'],
             content: '../content/images/homer.png'
         });
-        var mapItem = new MapItem({
-            zoomScaleFactor: 0.2,
+        var center = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5],
+            transform: Transform.rotateZ((Math.PI / 180) * -11)
+        });
+        var modifier = new MapItemStateModifier({
+            mapView: mapView,
             position: new google.maps.LatLng(52.3747158, 4.8986166)
-        }, mapView);
-        context.add(mapItem);
-        mapItem.show(surface, null, null, ((Math.PI / 180) * -5));
-        return mapItem;
+        });
+        context.add(modifier).add(center).add(surface);
+        return modifier;
     }
     
     /**
      * @method _createMarge
      * @private
      */
-    function _createMarge(context, mapView) {
+    function _createMarge(context, mapView, homer) {
+        var modifier = new MapItemModifier({
+            mapView: mapView,
+            position: new google.maps.LatLng(51.8538331, 5.3576616)
+        });
+        var center = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5]
+        });
         var surface = new ImageSurface({
             size: [true, true],
             classes: ['marge'],
             content: '../content/images/marge.png'
         });
-        var mapItem = new MapItem({
-            position: new google.maps.LatLng(51.4911905, 5.6651178)
-        }, mapView);
-        context.add(mapItem);
-        mapItem.show(surface);
-        return mapItem;
+        context.add(modifier).add(center).add(surface);
+        return modifier;
     }
 
     /**
@@ -186,21 +243,18 @@ define(function (require) {
      * @private
      */
     function _createInfoBox(context, mapView) {
-    
-        // Create renderable
+        var modifier = new Modifier({
+            align: [1, 1],
+            origin: [1, 1],
+            transform: Transform.translate(-30, -30, 0)
+        });
         var surface = new Surface({
             size: [200, 160],
-            content: 'Move the map by hand and see how MapItems sticks to the map.',
+            content: 'Move the map by hand and see how MapItems stick to the map.',
             classes: ['info']
         });
-
-        // Show on map
-        var mapItem = new MapItem({
-            position: new google.maps.LatLng(51.4387491, 5.4758324)
-        }, mapView);
-        context.add(mapItem);
-        mapItem.show(surface, { duration: 3000 });
-        return mapItem;
+        context.add(modifier).add(surface);
+        return modifier;
     }
     
     /**
@@ -208,22 +262,23 @@ define(function (require) {
      * @private
      */
     function _createPins(context, mapView) {
-    
-        // Create renderable
+        var modifier = new MapItemModifier({
+            mapView: mapView,
+            position: new google.maps.LatLng(51.4452133, 5.4806269),
+            zoomBase: 14,
+            zoomScale: 0.5
+        });
+        var center = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5]
+        });
         var surface = new ImageSurface({
-            size: [2, true],
+            size: [true, true],
             content: '../content/images/pins.png',
             classes: ['pins']
         });
-
-        // Show on map
-        var mapItem = new MapItem({
-            zoomScaleFactor: 4.0,
-            position: new google.maps.LatLng(51.4452133, 5.4806269)
-        }, mapView);
-        context.add(mapItem);
-        mapItem.show(surface);
-        return mapItem;
+        context.add(modifier).add(center).add(surface);
+        return modifier;
     }
     
     /**
@@ -231,21 +286,22 @@ define(function (require) {
      * @private
      */
     function _createSaucer(context, mapView) {
-    
-        // Create renderable
+        var modifier = new MapItemModifier({
+            mapView: mapView,
+            position: new google.maps.LatLng(51.443569, 5.446869),
+            zoomBase: 14,
+            zoomScale: 1
+        });
+        var center = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5]
+        });
         var surface = new ImageSurface({
-            size: [2, true],
+            size: [true, true],
             content: '../content/images/evoluon.png',
             classes: ['saucer']
         });
-
-        // Show on map
-        var mapItem = new MapItem({
-            zoomScaleFactor: 4.0,
-            position: new google.maps.LatLng(51.443569, 5.446869)
-        }, mapView);
-        context.add(mapItem);
-        mapItem.show(surface);
-        return mapItem;
+        context.add(modifier).add(center).add(surface);
+        return modifier;
     }
 });
